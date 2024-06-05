1746548855f2752127f99f6af115a3224aa6472b
Sangha Lee <sanghalee629@gmail.com>
2024-06-05 16:39:14 +0900

    complete settings UI section

---
+++ lib/features/settings/settings_screen.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Settings"),
+        ),
+        body: ListView(
+          children: [
+            CupertinoSwitch(
+                value: _notifications, onChanged: _onNotificationsChanged),
+            Switch(value: _notifications, onChanged: _onNotificationsChanged),
+            CheckboxListTile(
+              value: _notifications,
+              onChanged: _onNotificationsChanged,
+              activeColor: Colors.black,
+              checkColor: Colors.red,
+              title: const Text("Enable notifications"),
+            ),
+            ListTile(
+              onTap: () async {
+                final date = await showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1980),
+                    lastDate: DateTime(2030),
+                    initialDate: DateTime.now());
+                print(date);
+                final time = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                );
+                print(time);
+                final booking = await showDateRangePicker(
+                  context: context,
+                  firstDate: DateTime(1980),
+                  lastDate: DateTime(2030),
+                  builder: (context, child) {
+                    return Theme(
+                      data: ThemeData(
+                        appBarTheme: const AppBarTheme(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black,
+                        ),
+                      ),
+                      child: child!,
+                    );
+                  },
+                );
+                print(booking);
+              },
+              title: const Text("When is your birthday?"),
+            ),
+            const ListTile(
+              title: Text(
+                "About",
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              subtitle: Text("About this app...."),
+            ),
+            ListTile(
+              title: const Text("Log out (Android)"),
+              textColor: Colors.red,
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text("Are you sure?"),
+                    content: const Text("for real for real?"),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("Yes"),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("No"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Log out (iOS)"),
+              textColor: Colors.red,
+              onTap: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (context) => CupertinoAlertDialog(
+                    title: const Text("Are you sure?"),
+                    content: const Text("for real for real?"),
+                    actions: [
+                      CupertinoDialogAction(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("Yes"),
+                      ),
+                      CupertinoDialogAction(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("No"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Log out (iOS / Bottom)"),
+              textColor: Colors.red,
+              onTap: () {
+                showCupertinoModalPopup(
+                  context: context,
+                  builder: (context) => CupertinoAlertDialog(
+                    title: const Text("Are you sure?"),
+                    content: const Text("for real for real?"),
+                    actions: [
+                      CupertinoDialogAction(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("Yes"),
+                      ),
+                      CupertinoDialogAction(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("No"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text("Log out (iOS / Bottom / ActionSheet)"),
+              textColor: Colors.red,
+              onTap: () {
+                showCupertinoModalPopup(
+                  context: context,
+                  builder: (context) => CupertinoActionSheet(
+                    title: const Text("Are you sure?"),
+                    actions: [
+                      CupertinoActionSheetAction(
+                        onPressed: () {},
+                        child: const Text("Ayo"),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+            const AboutListTile(),
+          ],
+        ));
+  }
+}
